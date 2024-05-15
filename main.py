dc8fa94d131e615c4d158c7887c3cfcbe3066d9a
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-15 15:01:47 +0200

    Add links to full news source.

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ content = request.json()
 body = ""
 for article in content["articles"]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+        body = body + article["title"] + "\n" \
+      + str(article["description"]) \
+      + "\n" + str(article["url"]) + "\n\n"
+        + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
