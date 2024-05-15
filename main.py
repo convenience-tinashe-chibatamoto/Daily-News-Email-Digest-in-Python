73d16569ff2dd63aee5fbcd58f2f5b8215553ff7
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-15 15:20:13 +0200

    Setting filter: 8 max digest articles.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from send_email import send_email
 
 api_key = "0a16379b1db84fd6a69dd27ff09bafad"
-url = "https://newsapi.org/v2/everything?q=nvdia&" \
-      "sortBy=publishedAt&apiKey=" \
-      "0a16379b1db84fd6a69dd27ff09bafad&language=en"
+url = "https://newsapi.org/v2/everything?" \
+       "q=nvdia&" \
+      "sortBy=publishedAt" \
+      "&apiKey=0a16379b1db84fd6a69dd27ff09bafad" \
+      "&language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,7 +16,7 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:11]:
     if article["title"] is not None:
         body = body + article["title"] + "\n" \
       + str(article["description"]) \
